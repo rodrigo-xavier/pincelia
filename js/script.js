2eaae5d924219199821472ee57ec64b119e97b98
Rodrigo Xavier <rodrigomatiasxavier@gmail.com>
2024-09-11 03:32:23 -0300

    saving progress

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 function generateRandomColor() {
-    return '#' + Math.floor(Math.random()*16777215).toString(16);
+    let color;
+    do {
+        color = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+    } while (color === '#ffffff');
+
+    return color;
 }
 
 function setRandomGradient() {
