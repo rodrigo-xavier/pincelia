3743861c7aa2276d2ca099611c062c786dfa7b87
Rodrigo Xavier <rodrigomatiasxavier@gmail.com>
2024-09-11 12:29:40 -0300

    Testing github actions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+const profileName = import.meta.env.PROFILE_NAME;
+console.log('PROFILE NAME:', profileName);
+
+document.getElementById('profile-name').textContent = profileName;
+
+
 function generateRandomColor() {
     let color;
     do {
