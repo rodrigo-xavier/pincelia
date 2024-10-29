15978fcc85833ca84fa4613989c55ce90916baa6
Rodrigo Xavier <rodrigomatiasxavier@gmail.com>
2024-10-29 04:37:54 -0300

    translations

--- src/main.jsx
+++ src/main.jsx
@@ -7,18 +7,40 @@ import {
 } from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
+import React from "react";
 
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route>
-      <Route path="/us" element={<App />} />
-      <Route path="/pt" element={<App />} />
-      <Route path="*" element={<App />} />
-    </Route>
-  )
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          path: "/us",
+          element: <App />
+        },
+        {
+          path: "/pt",
+          element: <App />
+        },
+      ],
+    },
+  ],
 );
 
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route>
+//       <Route path="/us" element={<App />} />
+//       <Route path="/pt" element={<App />} />
+//       <Route path="*" element={<App />} />
+//     </Route>
+//   )
+// );
+
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
